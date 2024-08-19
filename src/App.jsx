87f765066d247380d2e30de730583638234ca385
SufiyanAK <Sufiyankhattak14@gmail.com>
2024-08-19 16:37:21 +0500

    completed redux Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { env } from './envImports/envVariables'
+import { useDispatch } from 'react-redux';
+import authServices from './appwriteServices/auth'
+import { login, logout } from './store/authSlice';
+import { Box, colors, Typography } from '@mui/material';
+import { Footer, Header } from './components';
 
 const App = () => {
 
-  console.log(env.projectId)
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    authServices.getUser()
+      .then((data) => {
+        if (data) {
+          dispatch(login(data))
+          return;
+        }
+        dispatch(logout())
+      })
+      .finally(() => setLoading(false))
+  }, []);
 
-  return (
+  return loading ? (
     <>
-      <h1>
-        App
-      </h1>
+      <Box>
+        <Header />
+        <Footer />
+      </Box>
     </>
-  )
+  ) : <Typography>Logout</Typography>
 }
 
 export default App
--- src/appwriteServices/auth.js
+++ src/appwriteServices/auth.js
@@ -1,6 +1,6 @@
 import { Client, Account, ID } from "appwrite";
 import { env } from "../envImports/envVariables";
-import { enqueueSnackbar } from 'notistack';
+// import { enqueueSnackbar } from 'notistack';
 
 export class AuthServices {
     client = new Client();
@@ -19,15 +19,16 @@ export class AuthServices {
             const userAccount = await this.account.create(ID.unique(), email, password, name);
 
             if (!userAccount) {
-                enqueueSnackbar('Account creation failed.', { variant: 'error' });
-                return;
+                // enqueueSnackbar('Account creation failed.', { variant: 'error' });
+                return console.log('Account creation failed.');
             }
 
-            enqueueSnackbar('Account created successfully.', { variant: 'success' });
+            // enqueueSnackbar('Account created successfully.', { variant: 'success' });
+            console.log('Account created successfully.');
             return userAccount;
         } catch (error) {
-            enqueueSnackbar('Please provide all fields.', { variant: 'error' });
-            console.error('Account creation error:', error);
+            // enqueueSnackbar('Please provide all fields.', { variant: 'error' });
+            console.error('Account creation error :: Please provide all fields.', error);
         }
     }
 
@@ -36,15 +37,16 @@ export class AuthServices {
             const userLogin = await this.account.createEmailPasswordSession(email, password);
 
             if (!userLogin) {
-                enqueueSnackbar('Incorrect Email or Password.', { variant: 'error' });
-                return;
+                // enqueueSnackbar('Incorrect Email or Password.', { variant: 'error' });
+                return console.log('Incorrect Email or Password.');
             }
 
-            enqueueSnackbar('Account login successful.', { variant: 'success' });
+            console.log('Account login successful.');
+            // enqueueSnackbar('Account login successful.', { variant: 'success' });
             return userLogin;
         } catch (error) {
-            enqueueSnackbar('Please provide all fields.', { variant: 'error' });
-            console.error('Login error:', error);
+            // enqueueSnackbar('Please provide all fields.', { variant: 'error' });
+            console.error('Login error :: Please provide all fields.', error);
         }
     }
 
@@ -52,13 +54,14 @@ export class AuthServices {
         try {
             const user = await this.account.get();
             if (!user) {
-                enqueueSnackbar('User not found.', { variant: 'error' });
+                // enqueueSnackbar('User not found.', { variant: 'error' });
+                console.log('User not found.');
                 return null;
             }
             return user;
         } catch (error) {
-            enqueueSnackbar('User not found.', { variant: 'error' });
-            console.error('Get user error:', error);
+            // enqueueSnackbar('User not found.', { variant: 'error' });
+            console.error(`Get user error :: User not found ${error.message}`);
             return null;
         }
     }
@@ -66,10 +69,11 @@ export class AuthServices {
     async userLogout() {
         try {
             await this.account.deleteSessions();
-            enqueueSnackbar('Logged out successfully.', { variant: 'success' });
+            // enqueueSnackbar('Logged out successfully.', { variant: 'success' });
+            console.log('Logged out successfully.');
         } catch (error) {
-            enqueueSnackbar('Logout failed.', { variant: 'error' });
-            console.error('Logout error:', error);
+            // enqueueSnackbar('Logout failed.', { variant: 'error' });
+            console.error('Logout error :: Logout failed.', error);
         }
     }
 }
